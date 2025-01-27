3d7f7fd3315cab9bee449eb1d71311d7e8c50f5e
TianTcl <chainathlim@gmail.com>
2025-01-26 22:56:53 -0500

    Upgrade to OOP

--- index.php
+++ index.php
@@ -6,47 +6,41 @@
 
 	if (isset($_REQUEST["gjc"])) header("Location: /v2/s/PBL/#/group/join/".$_REQUEST["gjc"]);
 	else header("Location: /v2/s/PBL/");
+	$APP_PAGE -> print -> head();
 ?>
-<!doctype html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<?php require($APP_RootDir."private/block/core/heading.php"); require($APP_RootDir."private/script/start/CSS-JS.php"); ?>
-		<style type="text/css">
-			
-		</style>
-		<script type="text/javascript">
-			// const TRANSLATION = location.pathname.substring(1).replace(/\/$/, "").replaceAll("/", "+");
-			$(document).ready(function() {
-				page.init();
-			});
-			const page = (function(d) {
-				const cv = { API_URL: AppConfig.APIbase + "" };
-				var sv = {inited: false};
-				var initialize = function() {
-					if (sv.inited) return;
+<style type="text/css">
+	
+</style>
+<script type="text/javascript">
+	// const TRANSLATION = location.pathname.substring(1).replace(/\/$/, "").replaceAll("/", "+");
+	$(document).ready(function() {
+		page.init();
+	});
+	const page = (function(d) {
+		const cv = { API_URL: AppConfig.APIbase + "" };
+		var sv = {inited: false};
+		var initialize = function() {
+			if (sv.inited) return;
 
-					sv.inited = true;
-				};
-				var myFunction = function() {
+			sv.inited = true;
+		};
+		var myFunction = function() {
 
-				};
-				return {
-					init: initialize,
-					myFunction
-				};
-			}(document));
-		</script>
-	</head>
-	<body>
-		<app name="main">
-			<?php require($APP_RootDir."private/block/core/top-panel/structure.php"); ?>
-			<main>
-				<section class="container">
-					<h2><?=$header["title"]?></h2>
+		};
+		return {
+			init: initialize,
+			myFunction
+		};
+	}(document));
+</script>
+<?php $APP_PAGE -> print -> nav(); ?>
+	<main>
+		<section class="container">
+			<h2><?=$header["title"]?></h2>
 
-				</section>
-			</main>
-			<?php require($APP_RootDir."private/block/core/material/main.php"); ?>
-		</app>
-	</body>
-</html>
+		</section>
+	</main>
+<?php
+	$APP_PAGE -> print -> materials();
+	$APP_PAGE -> print -> footer();
+?>
